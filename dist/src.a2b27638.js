a784fbe3e9145c8cd048179e6752b94f7ef2c8a2
Angie Mariscal <amariscalp12@gmail.com>
2022-09-17 19:42:09 -0500

    guardar

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -124,7 +124,7 @@ var gameReset = document.getElementById("gameReset");
 gameState = ["", "", "", "", "", "", "", "", ""], rightMoves = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]], winner = function winner() {
   return "El jugador ".concat(currentPlayer, " ha ganado!");
 }, finished = function finished() {
-  return "El juego ha terminado en empate!";
+  return "El juego ha terminado en un empate!";
 }, turnGame = function turnGame() {
   return "Turno del jugador ".concat(currentPlayer);
 };
@@ -144,7 +144,7 @@ function listeners() {
 function positionClick(clickedCellEvent) {
   var clickedCell = clickedCellEvent.target;
 
-  if (clickedCell.classList.contains('game-cell')) {
+  if (clickedCell.classList.contains('positionContainerStyle')) {
     var clickedCellIndex = Array.from(clickedCell.parentNode.children).indexOf(clickedCell);
 
     if (gameState[clickedCellIndex] !== '' || !gameActive) {
@@ -165,7 +165,7 @@ function resetPositionClick() {
   currentPlayer = "X";
   restartGameState();
   handleStatusDisplay(turnGame());
-  document.querySelectorAll('.game-cell').forEach(function (cell) {
+  document.querySelectorAll('.positionContainerStyle').forEach(function (cell) {
     return cell.innerHTML = "";
   });
 }
@@ -225,6 +225,108 @@ function restartGameState() {
 }
 
 main();
+/*const playerTurn = document.getElementById("playerTurn");
+const gameContainer = document.getElementById("gameContainer")
+const gameReset = document.getElementById("gameReset")
+
+gameState = ["", "", "", "", "", "", "", "", ""],
+rightMoves = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ],
+
+  winner = () => `El jugador ${currentPlayer} ha ganado!`,
+  finished = () => `El juego ha terminado en un empate!`,
+  turnGame = () => `Turno del jugador ${currentPlayer}`
+
+let gameActive = true,
+  currentPlayer = "O"
+
+function main() {
+  handleStatusDisplay(turnGame())
+  listeners()
+}
+function listeners() {
+    gameContainer.addEventListener('click',positionClick);
+    gameReset.addEventListener('click',resetPositionClick);
+}
+function positionClick(clickedCellEvent ) {
+    const clickedCell = clickedCellEvent.target
+    if (clickedCell.classList.contains('game-cell')) {
+      const clickedCellIndex = Array.from(clickedCell.parentNode.children).indexOf(clickedCell)
+      if (gameState[clickedCellIndex] !== '' || !gameActive) {
+        return false
+      }
+      handleCellPlayed(clickedCell, clickedCellIndex)
+      handleResultValidation()
+    }
+  }
+function handleStatusDisplay(message) {
+    playerTurn.innerHTML = message
+}
+function resetPositionClick() {
+  gameActive = true
+  currentPlayer = "X"
+  restartGameState()
+  handleStatusDisplay(turnGame())
+  document.querySelectorAll('.game-cell').forEach(cell => cell.innerHTML = "")
+}
+
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer 
+  clickedCell.innerHTML = currentPlayer 
+}
+
+function handleResultValidation() {
+  let roundWon = false
+  for (let i = 0; i < rightMoves.length; i++) { 
+    const winCondition = rightMoves[i]
+    let position1 = gameState[winCondition[0]],
+      position2 = gameState[winCondition[1]],
+      position3 = gameState[winCondition[2]] 
+
+    if (position1 === '' || position2 === '' || position3 === '') {
+      continue; 
+    }
+    if (position1 === position2 && position2 === position3) {
+      roundWon = true 
+      break
+    }
+  }
+
+  if (roundWon) {
+    handleStatusDisplay(winner())
+    gameActive = false
+    return
+  }
+
+  let roundDraw = !gameState.includes("") 
+  if (roundDraw) {
+    handleStatusDisplay(finished())
+    gameActive = false
+    return
+  }
+  handlePlayerChange()
+}
+
+function handlePlayerChange() {
+  currentPlayer = currentPlayer === "X" ? "O" : "X"
+  handleStatusDisplay(turnGame())
+}
+
+function restartGameState() {
+  let i = gameState.length
+  while (i--) {
+    gameState[i] = ''
+  }
+}
+main()*/
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -253,7 +355,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52203" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57975" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
